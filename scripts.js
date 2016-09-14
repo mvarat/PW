1b6f92dc6935f5c7b62a89cc213458119d75c5ee
Michelle <michelle@Macintosh-10.local>
2016-09-14 12:35:28 -0400

    adds mobile and desktop images

--- scripts.js
+++ scripts.js
@@ -35,12 +35,13 @@ $(function() {
   $("button").click(function(event){
     console.log(event.target);
     console.log("button clicked");
-    $("#modal").css("display","block");
+    $(event.target).next(".modal").css("display","block");
   });
+
   // modal scripts
-  $(".close").click(function(){
+  $(".close").click(function(event){
     console.log("modal closed");
-    $("#modal").css("display", "none");
+    $(".modal").css("display","none");
   });
 
   // !!!! THIS NEEDS WORK
@@ -59,7 +60,8 @@ var data = {
           sections: [
             {
                 title: "Creative Cabinets",
-                image: "./images/desktop/creative-cabinets.jpg",
+                imageDesktop: "./images/desktop/cc-desktop.jpg",
+                imageMobile: "./images/mobile/cc-mobile.jpg",
                 credit: "GE Appliances",
                 class: "cc",
                 features: [
@@ -67,8 +69,8 @@ var data = {
                         num: "cc-1",
                         xval: "23%",
                         yval: "64%",
-                        sponsored: "false",
-                        imageLink: "./images/mobile/cc-1-mobile.jpg",
+                        imageLinkMobile: "./images/mobile/cc-1-mobile.jpg",
+                        imageLinkDesktop: "./images/desktop/cc-1-desktop.jpg",
                         imageTitle: "Time for a Paint Over",
                         imageCaption: "No, we’re not saying you should wear your bikini as a top--we’re adults here. We’re referring to the L.A. gal’s take on sundresses punctuated with ankle boots. This works best with natural-colored booties, because they’ll make your legs appear longer rather than cut them off visually.",
                         shop: "Pantone ($13)",
@@ -78,8 +80,8 @@ var data = {
                         num: "cc-2",
                         xval: "25%",
                         yval: "10%",
-                        sponsored: "false",
-                        imageLink: "./images/mobile/cc-2-mobile.jpg",
+                        imageLinkMobile: "./images/mobile/cc-2-mobile.jpg",
+                        imageLinkDesktop: "./images/desktop/cc-2-desktop.jpg",
                         imageTitle: "Lighten Up",
                         imageCaption: "So you painted the cabinets, but now the upper doors are looking a little…heavy. Create dimension and brighten up the room by adding glass inserts to solid wood doors. It’s something a super-crafty person can do (read: you know your way around a handsaw), but the rest of us would call a handyman.",
                         shop: "IKEA ($89)",
@@ -90,7 +92,8 @@ var data = {
                         xval: "38%",
                         yval: "15%",
                         sponsored: "true",
-                        imageLink: "./images/mobile/cc-3-mobile.jpg",
+                        imageLinkMobile: "./images/mobile/cc-3-mobile.jpg",
+                        imageLinkDesktop: "./images/desktop/cc-3-desktop.jpg",
                         imageTitle: "Integrate Your Appliances",
                         imageCaption: "Gone are the days of clunky gadgets and appliances that stick out like sore thumbs. Today’s kitchen is a streamlined workspace. GE’s entire Slate collection is designed to blend in and not stand out. The elegant ranges and hoods are created in a stone-inspired hue and have a matte finish that melds seamlessly into any design.",
                         shop: "GE Glass Canopy Chimney Hood ($600 MSRP)",
@@ -101,7 +104,8 @@ var data = {
                         xval: "40%",
                         yval: "70%",
                         sponsored: "true",
-                        imageLink: "./images/mobile/cc-4-mobile.jpg",
+                        imageLinkMobile: "./images/mobile/cc-4-mobile.jpg",
+                        imageLinkDesktop: "./images/desktop/cc-4-desktop.jpg",
                         imageTitle: "Revel in Smudge-Proof Products",
                         imageCaption: "This is your kitchen command center. It’s the first thing people notice when they step into the room, and thanks to GE’s smudge-proof Slate collection, you can guarantee that your trusty oven and range stay fingerprint-free at all times. Not to mention, the seamless surface of their electric range makes it easy to wipe away spills.",
                         shop: "GE Double Oven Gas Range MSRP ($3,000 MSRP)",
@@ -111,8 +115,8 @@ var data = {
                        num: "cc-5",
                         xval: "60%",
                         yval: "48%",
-                        sponsored: "false",
-                        imageLink: "./images/mobile/cc-5-mobile.jpg",
+                        imageLinkMobile: "./images/mobile/cc-5-mobile.jpg",
+                        imageLinkDesktop: "./images/desktop/cc-5-desktop.jpg",
                         imageTitle: "Keep Grout Grime-Free",
                         imageCaption: "No, we’re not saying you should wear your bikini as a top--we’re adults here. We’re referring to the L.A. gal’s take on sundresses punctuated with ankle boots. This works best with natural-colored booties, because they’ll make your legs appear longer rather than cut them off visually.",
                         shop: "Marvis ($18)",
@@ -123,15 +127,16 @@ var data = {
             },
             {
               title: "Organized Pantry",
-              image: "./images/desktop/organized-pantry.jpg",
+              imageDesktop: "./images/desktop/op-desktop.jpg",
+              imageMobile: "./images/mobile/op-mobile.jpg",
               credit: "Getty Images",
               class: "op",
               features: [
                   {
                       xval: "34%",
                       yval: "50%",
-                      sponsored: "false",
-                      imageLink: "./images/mobile/op-1-mobile.jpg",
+                      imageLinkMobile: "./images/mobile/op-1-mobile.jpg",
+                      imageLinkDesktop: "./images/desktop/op-1-desktop.jpg",
                       imageTitle: "Think Beyond the Closet",
                       imageCaption: "A pantry doesn’t have to be tucked away. Instead, make it the focal point of your room by choosing a piece of furniture that you don’t mind putting on display. A flea-market find or even an IKEA Billy bookcase can be the perfect vessel--just make sure that ample shelving abounds.",
                       shop: "IKEA ($180)",
@@ -140,8 +145,6 @@ var data = {
                   {
                       xval: "50%",
                       yval: "25%",
-                      sponsored: "false",
-                      imageLink: "",
                       imageTitle: "Check In Seasonally",
                       imageCaption: "Twice a year, do a complete cabinet overhaul. First things first, clear everything out like you did when you KonMari’d your closet. Then make sure you’re fully stocked with all the essentials. Finally, hone in on the seasonal items. In the springtime, donate all those soup fixin’s you’ve acquired and replace them with your salad add-ons. Once autumn rolls around, ease up on the dried apricots and bring on the homemade applesauce.",
                       shop: "",
@@ -150,8 +153,8 @@ var data = {
                   {
                       xval: "71%",
                       yval: "24%",
-                      sponsored: "false",
-                      imageLink: "./images/mobile/op-3-mobile.jpg",
+                      imageLinkMobile: "./images/mobile/op-3-mobile.jpg",
+                      imageLinkDesktop: "./images/desktop/op-3-desktop.jpg",
                       imageTitle: "Embrace Containers",
                       imageCaption: "By storing small items in clear containers, you’ll keep everything in its place and be able to quickly see exactly what you have. Make sure your shelves are adjusted to the right height so that each container nestles into its intended spot. A lazy Susan is also extremely convenient for food storage because it utilizes every bit of even the deepest shelves.",
                       shop: "Food52 $(80)",
@@ -160,8 +163,8 @@ var data = {
                   {
                       xval: "66%",
                       yval: "68%",
-                      sponsored: "false",
-                      imageLink: "./images/mobile/op-4-mobile.jpg",
+                      imageLinkMobile: "./images/mobile/op-4-mobile.jpg",
+                      imageLinkDesktop: "./images/desktop/op-4-desktop.jpg",
                       imageTitle: "Visualize Your Meals",
                       imageCaption: "Focus on the food you make the most and group your go-to meal staples together at eye level (so put the box of angel hair next to the can of tomatoes and EVOO). You’ll be less likely to resort to takeout if dinner is within arm’s reach.",
                       shop: "",
@@ -171,7 +174,8 @@ var data = {
           },
           {
             title: "Clever Storage",
-            image: "./images/desktop/clever-storage.jpg",
+            imageDesktop: "./images/desktop/cs-desktop.jpg",
+            imageMobile: "./images/mobile/cs-mobile.jpg",
             credit: "GE Appliances",
             class: "cs",
             features: [
@@ -179,7 +183,8 @@ var data = {
                     xval: "40%",
                     yval: "30%",
                     sponsored: "true",
-                    imageLink: "./images/mobile/cs-1-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/cs-1-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/cs-1-desktop.jpg",
                     imageTitle: "Invest in Convenience",
                     imageCaption: "Meet the smartest fridge we know. GE’s new Slate French-door refrigerator has a built-in Keurig K-Cup brewing system, flexible shelves that bend and move depending on your storage needs, temperature adjusting from the GE Kitchen App and LED lights to spotlight ingredients so they’ll never get lost. Oh, did we mention the Keurig?",
                     shop: "GE Slate French-Door Refrigerator ($3,200 MSRP)",
@@ -188,8 +193,8 @@ var data = {
                 {
                     xval: "62%",
                     yval: "23%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/cs-2-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/cs-2-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/cs-2-desktop.jpg",
                     imageTitle: "Perfect Your Balancing Act",
                     imageCaption: "Bring balance to nooks and crannies with painted open shelving that puts your prettiest dishes and serving pieces on display. To create even more symmetry, install a mini mug rack below your shelf with a screwdriver and a few bronze cup hooks. Your guests will think they’ve walked into their very own B&B. Checkout time is never.",
                     shop: "Arrow ($2 each)",
@@ -198,8 +203,8 @@ var data = {
                 {
                     xval: "72%",
                     yval: "54%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/cs-3-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/cs-3-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/cs-3-desktop.jpg",
                     imageTitle: "Add Wood for Warmth",
                     imageCaption: "An all-marble or stainless-steel kitchen is super-cool, but it can come across as a smidgen austere. Warm up your space with natural elements, like reclaimed-wood cabinets and a stone backsplash, while still keeping that marble countertop you love. Word to the wise: You’ll want to protect those cabinets twice a year with a sealant.",
                     shop: "Food52 $(80)",
@@ -208,8 +213,8 @@ var data = {
                 {
                     xval: "75%",
                     yval: "70%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/cs-4-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/cs-4-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/cs-4-desktop.jpg",
                     imageTitle: "Learn the Lessons of Layering",
                     imageCaption: "A room designed almost entirely in reclaimed wood could easily begin to feel repetitive. The trick is to keep your color palette the same while making sure everything differs slightly: the floor from the cabinets, the cabinets from the stools, the stools from the soap dispenser--you get the idea. Once all the elements are together, the colors and textures will make it feel cohesive, but you won’t be stuck in a wood-washed box.",
                     shop: "Target (from $54)",
@@ -219,15 +224,16 @@ var data = {
           },
           {
             title: "Breakfast Nook",
-            image: "./images/desktop/breakfast-nook.jpg",
+            imageDesktop: "./images/desktop/bn-desktop.jpg",
+            imageMobile: "./images/mobile/bn-mobile.jpg",
             credit: "Amber Interiors",
             class: "bn",
             features: [
                 {
                     xval: "20%",
                     yval: "70%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/bn-1-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/bn-1-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/bn-1-desktop.jpg",
                     imageTitle: "Mix and Match Seating",
                     imageCaption: "Isn’t it your dream to have people linger forever and ever over breakfast? (No? Just us?) Install a built-in bench to create your own dining nook or opt for a stand-alone style that works just as well (and gives you flexibility should you decide to switch things up down the road).",
                     shop: "Pottery Barn (from $1,097)",
@@ -236,8 +242,8 @@ var data = {
                 {
                     xval: "25%",
                     yval: "93%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/bn-2-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/bn-2-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/bn-2-desktop.jpg",
                     imageTitle: "Tie It Together",
                     imageCaption: "An oriental rug in the kitchen? You bet. Not only does it turn this corner of the room into a cozy eating space, but it hides virtually anything (anything!) that could go a-tumbling off the table. That includes you, wine.",
                     shop: "Urban Outfitters ($89)",
@@ -246,8 +252,8 @@ var data = {
                 {
                     xval: "30%",
                     yval: "35%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/bn-3-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/bn-3-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/bn-3-desktop.jpg",
                     imageTitle: "Infuse Texture",
                     imageCaption: "Kitchens can feel so austere--marble, plastic, silicone, quartz, you get the idea. But adding in a raw chair, a nubby pillow and a plush rug make it more welcoming",
                     shop: "Rebecca Atwood ($309)",
@@ -256,8 +262,8 @@ var data = {
                 {
                     xval: "55%",
                     yval: "40%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/bn-4-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/bn-4-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/bn-4-desktop.jpg",
                     imageTitle: "Add a Hint of Citrus",
                     imageCaption: "Sunshine-colored lemons needn’t be tucked away in a fridge (where they actually lose flavor). Keep them out on the breakfast table and make your produce serve double duty. Plus, lemons are the new Xanax.",
                     shop: "",
@@ -266,8 +272,8 @@ var data = {
                 {
                     xval: "80%",
                     yval: "12%",
-                    sponsored: "false",
-                    imageLink: "./images/mobile/bn-5-mobile.jpg",
+                    imageLinkMobile: "./images/mobile/bn-5-mobile.jpg",
+                    imageLinkDesktop: "./images/desktop/bn-5-desktop.jpg",
                     imageTitle: "Start Out White",
                     imageCaption: "The beauty of this kitchen’s styling is the expert mix of patterns (ticking stripes with shibori) and genres (mid-century chairs around an industrial table). And it all works together because it’s rooted in a neutral foundation with white cabinetry, white walls and a clean sea-glass-tiled backsplash.",
                     shop: "Benjamin Moore (price upon request)",
